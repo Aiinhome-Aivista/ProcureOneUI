d8f8ec23c6642d57b94152565d5b645efcd31439
Suvojit Mondal <mondalsuvojit160@gmail.com>
2025-11-04 17:34:56 +0530

    code past for all four registration page

--- src/app/features/auth/register/components/steps/bank-identity-verification/bank-identity-verification.ts
+++ src/app/features/auth/register/components/steps/bank-identity-verification/bank-identity-verification.ts
@@ -7,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './bank-identity-verification.css',
 })
 export class BankIdentityVerification {
+  uploadedFiles: any = {
+    cancelCheque: null,
+    bankStatement: null,
+    verifyLetter: null
+  };
+
+  onFileChange(field: string, event: any) {
+    const file: File = event.target.files[0];
+
+    if (!file) return;
+
+    if (file.type !== "application/pdf") {
+      alert("Only PDF files are allowed");
+      return;
+    }
+
+    const maxSize = 5 * 1024 * 1024;
+    if (file.size > maxSize) {
+      alert("File size should not exceed 5MB");
+      return;
+    }
+
+    this.uploadedFiles[field] = file;
+  }
+
+  removeFile(field: string) {
+    this.uploadedFiles[field] = null;
+  }
 
 }
