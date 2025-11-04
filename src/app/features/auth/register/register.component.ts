76d2553b8af43fe7f96f7a776aaf0d965809373c
Rohan Das <rohandas8944@gmail.com>
2025-11-04 19:46:48 +0530

    add

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { StepIndicatorComponent } from './components/step-indicator.component';
+
 import { Sidebar } from './components/sidebar/sidebar';
 import { IdentityDetails } from './components/steps/identity-details/identity-details';
 import { BusinessTaxRegistration } from './components/steps/business-tax-registration/business-tax-registration';
 import { BankIdentityVerification } from './components/steps/bank-identity-verification/bank-identity-verification';
 import { FinancialDocuments } from './components/steps/financial-documents/financial-documents';
+import { StepIndicatorComponent } from './components/step-indicator/step-indicator.component';
 
 @Component({
   selector: 'app-register',
