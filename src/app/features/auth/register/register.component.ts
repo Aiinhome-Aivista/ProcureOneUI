34571adcb146738f5256af2494f2a47aefa711f6
PSpabitra <sarkarpabi42@gmail.com>
2025-11-04 18:20:34 +0530

    safe

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,25 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { StepIndicatorComponent } from './components/step-indicator.component';
+
 import { Sidebar } from './components/sidebar/sidebar';
 import { IdentityDetails } from './components/steps/identity-details/identity-details';
 import { BusinessTaxRegistration } from './components/steps/business-tax-registration/business-tax-registration';
 import { BankIdentityVerification } from './components/steps/bank-identity-verification/bank-identity-verification';
 import { FinancialDocuments } from './components/steps/financial-documents/financial-documents';
+import { StepIndicatorComponent } from "./components/step-indicator/step-indicator.component";
 
 @Component({
   selector: 'app-register',
   imports: [
     CommonModule,
     RouterModule,
-    StepIndicatorComponent,
     Sidebar,
     IdentityDetails,
     BusinessTaxRegistration,
     BankIdentityVerification,
-    FinancialDocuments
-  ],
+    FinancialDocuments,
+    StepIndicatorComponent
+],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
