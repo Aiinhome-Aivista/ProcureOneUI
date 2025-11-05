51522e1dad9e4cf0f66f544abf31cf739d74f418
Pinki Paul <pp3785828@gmail.com>
2025-11-05 16:35:03 +0530

    design final submission page

---
+++ src/app/features/auth/register/components/steps/final-submission/final-submission.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-final-submission',
+  imports: [],
+  templateUrl: './final-submission.html',
+  styleUrl: './final-submission.css',
+})
+export class FinalSubmission {
+
+}
