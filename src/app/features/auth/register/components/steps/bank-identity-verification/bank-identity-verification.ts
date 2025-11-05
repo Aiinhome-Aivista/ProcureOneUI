49efed687bf1d5c3bc193dd8aebfe58364015d8d
Rohan Das <rohandas8944@gmail.com>
2025-11-05 18:19:37 +0530

    add

--- src/app/features/auth/register/components/steps/bank-identity-verification/bank-identity-verification.ts
+++ src/app/features/auth/register/components/steps/bank-identity-verification/bank-identity-verification.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { Loader } from "../../../../../../shared/components/loader/loader";
 
 @Component({
   selector: 'app-bank-identity-verification',
-  imports: [Loader],
+  imports: [],
   templateUrl: './bank-identity-verification.html',
   styleUrl: './bank-identity-verification.css',
 })
